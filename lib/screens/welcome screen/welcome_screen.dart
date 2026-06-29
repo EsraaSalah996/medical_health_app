f7e652f2cdf3b1a00019ddaf1d82d6f60b221ef8
heba rostom <heba.muhamed220@gmail.com>
2026-06-29 03:03:09 +0200

    splash screen and welcome screen done

---
+++ lib/screens/welcome screen/welcome_screen.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'package:medical_health_app/screens/login_screen/widgets/custom_login_button.dart';
+import 'package:medical_health_app/screens/welcome%20screen/widgets/custom_sign_up_button.dart';
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0), 
+          child: Column(
+            children: [
+              const Spacer(flex: 2), 
+              
+              Image.asset(
+                'assets/images/welcome.png',
+                width: 120, 
+              ),
+              const SizedBox(height:60),
+              
+              Image.asset(
+                'assets/images/welcome text.png',
+                width: 267,
+              ),              
+              
+              const SizedBox(height:30),              
+              CustomLoginButton(title: "Log In", onTap:(){}),
+
+              const SizedBox(height:5),
+              
+              CustomsignupButton(title: "Sign Up", onTap:(){}),
+              const Spacer(flex: 1), 
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
